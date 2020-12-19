4e72a5a025b7539d7e1e814db1706784d8768087
kjj924 <dkansk924@naver.com>
2020-12-19 21:57:59 +0900

    Member 가져올시 OrderList 에서 menu 가 늘어날시 N+1 문제 발생 -> 해결

--- src/main/java/hair_shop/demo/member/MemberRepository.java
+++ src/main/java/hair_shop/demo/member/MemberRepository.java
@@ -2,9 +2,15 @@ package hair_shop.demo.member;
 
 
 import hair_shop.demo.domain.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.Entity;
 
 public interface MemberRepository  extends JpaRepository<Member, Long> {
+
+    @Query("select a from Member a join fetch a.orderList s join fetch s.menus")
     Member findByPhone(String phoneNumber);
 
     boolean existsByPhone(String phone);
