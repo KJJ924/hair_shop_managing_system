f3f7fc388aa8294f212406802e2496a660f8febd
kjj924 <dkansk924@naver.com>
2020-12-24 22:14:21 +0900

    OrderController Bug fix

--- src/main/java/hair_shop/demo/order/OrderController.java
+++ src/main/java/hair_shop/demo/order/OrderController.java
@@ -1,7 +1,5 @@
 package hair_shop.demo.order;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import hair_shop.apiMessage.ApiResponseMessage;
 import hair_shop.demo.designer.DesignerController;
 import hair_shop.demo.designer.DesignerRepository;
@@ -12,28 +10,25 @@ import hair_shop.demo.menu.MenuController;
 import hair_shop.demo.menu.MenuRepository;
 import hair_shop.demo.order.form.OrderForm;
 import hair_shop.demo.order.form.Payment;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class OrderController {
     public static final String NOT_FOUND_ORDER ="해당하는 예약이 없음";
 
     private final OrderService orderService;
-    private final ObjectMapper objectMapper;
     private final MemberRepository memberRepository;
     private final MenuRepository menuRepository;
     private final DesignerRepository designerRepository;
 
     @PostMapping("/order")
-    @ResponseBody
     public ResponseEntity<Object> createOrder(@RequestBody OrderForm orderForm){
         if(!memberRepository.existsByPhone(orderForm.getMemberPhoneNumber())){
             return ApiResponseMessage.error(orderForm.getMemberPhoneNumber(), MemberController.NOT_FOUND_MEMBER);
@@ -49,7 +44,6 @@ public class OrderController {
     }
 
     @GetMapping("/month/{year}/{month}")
-    @ResponseBody
     public ResponseEntity<Object> getMonthData(@PathVariable int year, @PathVariable int month){
         LocalDateTime standardMonth = LocalDateTime.of(year, month, 1, 0, 0);
         LocalDateTime plusMonth = standardMonth.plusMonths(1);
@@ -58,12 +52,10 @@ public class OrderController {
 
     @GetMapping("/week/{year}/{month}/{baseDate}/{targetDay}")
     public ResponseEntity<Object> getWeekData(@PathVariable int year,@PathVariable int month
-            , @PathVariable int baseDate, @PathVariable int targetDay) throws JsonProcessingException {
+            , @PathVariable int baseDate, @PathVariable int targetDay){
         LocalDateTime standardDay = LocalDateTime.of(year,month,baseDate,0,0);
         LocalDateTime plusDay = standardDay.plusDays(targetDay-baseDate).plusHours(24);
-        Map<Integer, List<OrderTable>> weekData = orderService.getWeekData(standardDay, plusDay);
-        String content = objectMapper.writeValueAsString(weekData);
-        return ResponseEntity.ok(content);
+        return ResponseEntity.ok(orderService.getWeekData(standardDay, plusDay));
     }
 
     @PutMapping("/order/payment")
