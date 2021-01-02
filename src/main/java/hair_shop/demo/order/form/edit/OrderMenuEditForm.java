dc3fadda55c9676f8cba748ae8331dabf7773447
kjj924 <dkansk924@naver.com>
2021-01-02 21:43:50 +0900

    OrderMenuEditForm 예외 추가.

--- src/main/java/hair_shop/demo/order/form/edit/OrderMenuEditForm.java
+++ src/main/java/hair_shop/demo/order/form/edit/OrderMenuEditForm.java
@@ -2,8 +2,13 @@ package hair_shop.demo.order.form.edit;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class OrderMenuEditForm {
+
+    @NotNull
     String menuName;
+
     Long orderId;
 }
