828c47c340eee8aac6ba78471bc18c0e29d7816c
jaejoon <dkansk924@naver.com>
2021-05-31 20:02:19 +0900

    [refactor] findByPhoneNumber 메서드을 제공함으로써 불필요한 로직 삭제 및 정리

--- src/main/java/hair_shop/demo/modules/member/membership/service/MemberShipService.java
+++ src/main/java/hair_shop/demo/modules/member/membership/service/MemberShipService.java
@@ -1,12 +1,12 @@
 package hair_shop.demo.modules.member.membership.service;
 
 import hair_shop.demo.Infra.apiMessage.ApiResponseMessage;
-import hair_shop.demo.modules.member.domain.Member;
-import hair_shop.demo.modules.member.membership.repository.MemberShipRepository;
-import hair_shop.demo.modules.member.membership.domain.MemberShip;
 import hair_shop.demo.modules.member.MemberController;
-import hair_shop.demo.modules.member.MemberRepository;
+import hair_shop.demo.modules.member.MemberService;
+import hair_shop.demo.modules.member.domain.Member;
+import hair_shop.demo.modules.member.membership.domain.MemberShip;
 import hair_shop.demo.modules.member.membership.dto.request.MemberShipForm;
+import hair_shop.demo.modules.member.membership.repository.MemberShipRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -17,28 +17,28 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class MemberShipService {
 
-    private final MemberRepository memberRepository;
     private final MemberShipRepository memberShipRepository;
+    private final MemberService memberService;
 
     public ResponseEntity<Object> createMemberShip(MemberShipForm form) {
-        Member member = memberRepository.findByPhone(form.getPhone());
+
+        Member member = memberService.findByPhone(form.getPhone());
 
         //TODO [refactor]  1. 회원이 존재하는지 ? 2.  존재하는데 이미 회원권이 존재하는 회원인지 ?
         // 문제점 1. 모든 메세지 처리가 ApiResponseMessage 통해 처리되고 있음
         // 문제점 2. 반환 모델이 존재하지않음(DTO)
         // 해결방안
         // 반환 DTO 생성 및 GlobalExceptionController 통한 에러관리
-        if (member == null) {
-            return ApiResponseMessage.error(form.getPhone(), MemberController.NOT_FOUND_MEMBER);
-        } else if (member.isMemberShip()) {
-            return ApiResponseMessage.error("alreadyMemberShip", MemberController.ALREADY_MEMBERSHIP);
+        if (member.isMemberShip()) {
+            return ApiResponseMessage
+                .error("alreadyMemberShip", MemberController.ALREADY_MEMBERSHIP);
         }
 
         //TODO [refactor] 위에 존재하고 있는 예외가 다 통과한다면 회원권이 생성되어 등록된다.
         saveMemberShip(member, form.getPoint());
-
         return ApiResponseMessage.success("회원권이 생성되었습니다.");
     }
+
 
     private void saveMemberShip(Member member, int point) {
         MemberShip save = memberShipRepository.save(MemberShip.create(point));
@@ -46,10 +46,9 @@ public class MemberShipService {
     }
 
     public ResponseEntity<Object> addPoint(MemberShipForm form) {
-        Member member = memberRepository.findByPhone(form.getPhone());
-        if (member == null) {
-            return ApiResponseMessage.error(form.getPhone(), MemberController.NOT_FOUND_MEMBER);
-        } else if (!member.isMemberShip()) {
+        Member member = memberService.findByPhone(form.getPhone());
+
+        if (!member.isMemberShip()) {
             return ApiResponseMessage.error("No MemberShip", MemberController.NOT_MEMBERSHIP);
         }
         member.addPoint(form.getPoint());
