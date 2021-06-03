31ff3622c0c5006538282658a950cc093c688e93
jaejoon <dkansk924@naver.com>
2021-06-03 18:10:09 +0900

    [refactor] menu

--- src/main/java/hair_shop/demo/modules/menu/domain/Menu.java
+++ src/main/java/hair_shop/demo/modules/menu/domain/Menu.java
@@ -1,26 +1,38 @@
 package hair_shop.demo.modules.menu.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @EqualsAndHashCode(of = "id")
-@Getter @Setter
-@Builder
-@AllArgsConstructor @NoArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Menu {
 
-    @JsonIgnore
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true ,nullable = false)
+    @Column(unique = true, nullable = false)
     private String name;
 
     private int price;
+
+
+    public void editPrice(int price) {
+        this.price = price;
+    }
+
+    public void changeName(String newName) {
+        this.name = newName;
+    }
 }
