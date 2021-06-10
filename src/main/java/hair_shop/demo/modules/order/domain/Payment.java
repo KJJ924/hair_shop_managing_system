4bdfe6b7ac6d523db6de724df6cadad15c76d2c8
jaejoon <dkansk924@naver.com>
2021-06-10 23:53:50 +0900

    [chore]  payment filed edit

--- src/main/java/hair_shop/demo/modules/order/domain/Payment.java
+++ src/main/java/hair_shop/demo/modules/order/domain/Payment.java
@@ -4,7 +4,7 @@ public enum  Payment {
     CASH("현금"),POINT("포인트"),NOT_PAYMENT("미결제"),
     CASH_AND_POINT("현금+포인트");
 
-    private String value;
+    private final String value;
 
     Payment(String value) {
         this.value = value;
