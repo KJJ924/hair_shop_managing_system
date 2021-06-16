3800e01b4731d039e35e9b90bbfc352c94a85460
jaejoon <dkansk924@naver.com>
2021-06-16 20:27:03 +0900

    [feat] SwaggerConfig

---
+++ src/main/java/hair_shop/demo/infra/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package hair_shop.demo.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author dkansk924@naver.com
+ * @since 2021/06/16
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Hair-Shop Swagger Documentation")
+            .description("Hair-Shop Documentation")
+            .version("1.0")
+            .license("License Version 1.0")
+            .licenseUrl("https://github.com/KJJ924/hair_shop_managing_system")
+            .build();
+    }
+}
+
