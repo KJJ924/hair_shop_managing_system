2db54023ab861dec3c1d2b5ea130c7334e9fcad1
jaejoon <dkansk924@naver.com>
2021-06-20 16:54:27 +0900

    [REFACTOR] payment constant-specific method implementation 으로 변경

--- src/main/java/hair_shop/demo/modules/order/domain/Payment.java
+++ src/main/java/hair_shop/demo/modules/order/domain/Payment.java
@@ -1,44 +1,47 @@
 package hair_shop.demo.modules.order.domain;
 
 import hair_shop.demo.modules.order.payment.dto.response.ResponsePayment;
-import java.util.function.Function;
 
 public enum Payment {
-    CASH("현금", order -> {
-        order.cashPayment();
-        return ResponsePayment.builder()
-            .paymentType("현금")
-            .memberName(order.getMemberName())
-            .phoneNumber(order.getMemberPhone())
-            .build();
-    }),
-
-    POINT("포인트", order -> {
-        int remainingPoint = order.pointPayment();
-
-        return ResponsePayment.builder()
-            .paymentType("포인트")
-            .memberName(order.getMemberName())
-            .phoneNumber(order.getMemberPhone())
-            .holdingPoint(order.totalPrice() + remainingPoint)
-            .paymentAmount(order.totalPrice())
-            .remainingPoint(remainingPoint)
-            .build();
-    }
-    ),
-
-    NOT_PAYMENT("미결제", order -> ResponsePayment.builder().build());
+    CASH("현금"){
+        @Override
+        public ResponsePayment process(Order order) {
+            order.cashPayment();
+            return ResponsePayment.builder()
+                .paymentType(this.getValue())
+                .memberName(order.getMemberName())
+                .phoneNumber(order.getMemberPhone())
+                .build();
+        }
+    },
+    POINT("포인트") {
+        @Override
+        public ResponsePayment process(Order order) {
+            int remainingPoint = order.pointPayment();
+            return ResponsePayment.builder()
+                .paymentType(this.getValue())
+                .memberName(order.getMemberName())
+                .phoneNumber(order.getMemberPhone())
+                .holdingPoint(order.totalPrice() + remainingPoint)
+                .paymentAmount(order.totalPrice())
+                .remainingPoint(remainingPoint)
+                .build();
+        }
+    },
+    NOT_PAYMENT("미결제"){
+        @Override
+        public ResponsePayment process(Order order) {
+            return ResponsePayment.builder().build();
+        }
+    };
 
     private final String value;
-    private final Function<Order, ResponsePayment> paymentFunction;
 
-    Payment(String value, Function<Order, ResponsePayment> paymentFunction) {
+    Payment(String value) {
         this.value = value;
-        this.paymentFunction = paymentFunction;
     }
-    public ResponsePayment process(Order order) {
-        return paymentFunction.apply(order);
-    }
+
+    public abstract ResponsePayment process(Order order);
 
     public String getValue() {
         return value;
