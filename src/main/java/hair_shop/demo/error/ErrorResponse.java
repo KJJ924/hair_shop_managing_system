694eedfb5d882915c9d81d80264aa96a834d1c71
jaejoon <dkansk924@naver.com>
2021-06-25 15:41:42 +0900

    [Update] MethodArgumentNotValidException ErrorHandler 추가

--- src/main/java/hair_shop/demo/error/ErrorResponse.java
+++ src/main/java/hair_shop/demo/error/ErrorResponse.java
@@ -1,5 +1,10 @@
 package hair_shop.demo.error;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Builder;
 import lombok.Getter;
 
 /**
@@ -9,14 +14,25 @@ import lombok.Getter;
 
 
 @Getter
+@JsonInclude(Include.NON_NULL)
 public class ErrorResponse {
 
     private final int statusCode;
     private final String message;
+    private List<ErrorField> errors;
+    private final LocalDateTime timestamp = LocalDateTime.now();
 
     private ErrorResponse(int statusCode, String message) {
         this.statusCode = statusCode;
         this.message = message;
+    }
+
+    @Builder
+    public ErrorResponse(int statusCode, String message,
+        List<ErrorField> errors) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.errors = errors;
     }
 
     public static ErrorResponse of(int statusCode, String message) {
--- src/main/java/hair_shop/demo/error/GlobalExceptionController.java
+++ src/main/java/hair_shop/demo/error/GlobalExceptionController.java
@@ -1,7 +1,13 @@
 package hair_shop.demo.error;
 
 import hair_shop.demo.error.exception.BusinessException;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,5 +26,21 @@ public class GlobalExceptionController {
         ErrorResponse response = ErrorResponse
             .of(errorCode.getStatus().value(), errorCode.getMessage());
         return ResponseEntity.status(errorCode.getStatus()).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> dataValidateError(MethodArgumentNotValidException e) {
+        ErrorResponse response = ErrorResponse.builder()
+            .statusCode(HttpStatus.BAD_REQUEST.value())
+            .message("입력값이 올바르지 않습니다.")
+            .errors(createMessage(e))
+            .build();
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    private List<ErrorField> createMessage(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        return fieldErrors.stream().map(ErrorField::mapper).collect(Collectors.toList());
     }
 }
